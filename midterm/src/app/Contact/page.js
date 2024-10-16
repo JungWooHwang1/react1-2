e488aae100728593210038c8da3d163f71433596
JungWooHwang <xhdpc123@naver.com>
2024-10-16 14:34:22 +0900

    세번쨰 커밋

--- midterm/src/app/Contact/page.js
+++ midterm/src/app/Contact/page.js
@@ -1,7 +1,29 @@
+import { NavLink } from "react-router-dom";
+
 export default function Contact() {
     return (
-      <div className={styles.page}>
-          <h1>황정우 202030435 C:\Users\user\Desktop\react1-2\midterm</h1>
+        <div className={styles.page}>
+        <div>
+          <NavLink to="/">
+            Home
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/About">
+            Recommend
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/Contact">
+            Search
+          </NavLink>
+        </div>
+        <div>
+          <NavLink to="/slug">
+            MyPage
+          </NavLink>
+        </div>       
+        <h1>황정우 202030435 C:\Users\user\Desktop\react1-2\midterm</h1>
       </div>
     );
   }
--- midterm/src/app/page.js
+++ midterm/src/app/page.js
@@ -1,10 +1,37 @@
-import Image from "next/image";
+import { NavLink } from "react-router-dom";
 import styles from "./page.module.css";
-
 export default function Home() {
+
+  // <Route path = "/" component = {Home}> </Route>
+
+
   return (
     <div className={styles.page}>
-        <h1>황정우 202030435 C:\Users\user\Desktop\react1-2\midterm</h1>
+      <div>
+        <NavLink to="/">
+          Home
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to="/About">
+          Recommend
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to="/Contact">
+          Search
+        </NavLink>
+      </div>
+      <div>
+        <NavLink to="/slug">
+          MyPage
+        </NavLink>
+      </div>       
+      <h1>황정우 202030435 C:\Users\user\Desktop\react1-2\midterm</h1>
     </div>
+ 
+
+
+
   );
 }
