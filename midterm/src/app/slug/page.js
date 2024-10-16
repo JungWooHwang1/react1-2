d5a8d036cca0c06c78b12f633e6986ea02f689cc
JungWooHwang <xhdpc123@naver.com>
2024-10-16 14:16:09 +0900

    두번째 커밋

---
+++ midterm/src/app/slug/page.js
@@ -0,0 +1,7 @@
+export default function slug() {
+    return (
+      <div className={styles.page}>
+          <h1>황정우 202030435 C:\Users\user\Desktop\react1-2\midterm</h1>
+      </div>
+    );
+  }
